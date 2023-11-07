e3c0cdc99c4db69a347473e22a049d285270b052
DanielMunoz4190 <a01641792@itesm.mx>
2023-11-07 17:19:11 -0600

    lista de acceso y validacion de tokens

---
+++ models/tokenModel.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const tokenSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+    required: true,
+  },
+  token: { type: String, required: true },
+  createdAt: { type: Date, required: true, default: Date.now, expires: 43200 },
+});
+
+const Token = mongoose.model("Tokens", tokenSchema);
+
+export default Token;
