2f667b8768c31d24df595f4612c7b751c3da3831
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-12-01 17:40:21 -0600

    Add imageURL field to createPet function

--- models/amezcuaPetModel.js
+++ models/amezcuaPetModel.js
@@ -4,6 +4,7 @@ const petSchema = new mongoose.Schema({
   name: { type: String },
   age: { type: String },
   weight: { type: String },
+  imageURL: { type: String },
 });
 
 const Pet = mongoose.model("amezcuaPets", petSchema);
